9be7cf935aba1745adc4672aba66579f0193c48b
v1cferr <dev.victorferreira@gmail.com>
2024-10-26 16:04:00 -0300

    feat(Card): temp loading

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-
 interface CardProps {
   ipAddress: string;
   location: string;
   timezone: string;
   isp: string;
+  loading: boolean;
 }
 
 interface CardItemProps {
@@ -24,12 +23,27 @@ export default function Card({
   location,
   timezone,
   isp,
+  loading,
 }: CardProps) {
+  if (loading) {
+    return (
+      // TODO: É melhor adicionar um skeleton
+      <div className="bg-white rounded-xl p-4 w-full max-w-3xl shadow-xl flex flex-col gap-y-6 items-center">
+        <div className="relative inline-flex">
+          <div className="w-8 h-8 bg-blue-500 rounded-full"></div>
+          <div className="w-8 h-8 bg-blue-500 rounded-full absolute top-0 left-0 animate-ping"></div>
+          <div className="w-8 h-8 bg-blue-500 rounded-full absolute top-0 left-0 animate-pulse"></div>
+        </div>
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
+  }
+
   return (
-    <ol className="bg-white rounded-xl p-4 w-full max-w-3xl shadow-xl flex flex-col gap-6 items-center ">
+    <ol className="bg-white rounded-xl p-4 w-full max-w-3xl shadow-xl flex flex-col gap-y-6 items-center ">
       <CardItem title="IP Address" value={ipAddress} />
       <CardItem title="Location" value={location} />
-      <CardItem title="Timezone" value={timezone} />
+      <CardItem title="Timezone" value={`UTC ${timezone}`} />
       <CardItem title="ISP" value={isp} />
     </ol>
   );

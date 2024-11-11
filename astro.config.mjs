21e1c5b5e1469a28629f7c5e20365898dcbb42c0
v1cferr <dev.victorferreira@gmail.com>
2024-11-11 13:28:21 -0300

    chore: env to build and deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: "static",
+  outDir: "./dist",
+  site: "https://v1cferr.github.io/ip-address-tracker/",
 });

ebcbfa3c4758fe788f7bda936bc5f5f266843688
v1cferr <dev.victorferreira@gmail.com>
2024-11-15 14:26:52 -0300

    actions: SSR and install node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "static",
+  output: "server",
   outDir: "./dist",
   site: "https://v1cferr.github.io/ip-address-tracker/",
+  adapter: node({
+    mode: "standalone",
+  }),
 });

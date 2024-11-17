044f277ea0b1633147114f7190751a9c6dd6e3f4
v1cferr <dev.victorferreira@gmail.com>
2024-11-17 18:38:59 -0300

    style: add my logo on top right

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import Input from "./components/Input";
 import Map from "./components/Map";
 import { LocationProvider } from "./context/LocationContext";
+import logo from "./assets/v1cferr-logo.svg";
 import "./App.css";
 
 export default function App() {
   return (
     <LocationProvider>
+      <a
+        href="https://github.com/v1cferr/ip-address-tracker"
+        target="_blank"
+        rel="noopener noreferrer">
+        <img
+          src={logo}
+          alt="@v1cferr - Logo"
+          className="fixed top-2.5 right-2.5 w-10 h-10 z-30 transition-all duration-300 ease-in-out hover:cursor-pointer bg-[#FDFDFD] rounded-xl p-2 shadow-md"
+        />
+      </a>
       <main className="relative flex flex-col items-center justify-center h-screen w-full">
         <div className="absolute top-0 w-full z-20 flex flex-col items-center justify-center gap-y-6 mt-6 px-8">
           <h1>
